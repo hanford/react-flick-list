434a3c636949ee05c08ebb19bc6419897df96281
hanford <jackhanford@gmail.com>
2017-08-27 20:32:56 -0700

    add example

---
+++ examples/simple/src/kinetic.js
@@ -0,0 +1,172 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import document from 'global/document'
+import window from 'global/window'
+
+class Kinetic extends PureComponent {
+
+  min = 0
+  max = 0
+  reference = 0
+  velocity = 0
+  offset = 0
+  amplitude = 0
+  timestamp = null
+  ticker = null
+  pressed = false
+
+  static defaultProps = {
+    broadcast: () => {},
+    element: document.body,
+    direction: 'y',
+    allowTaps: true,
+    max: 0,
+    min: 0
+  }
+
+  componentDidMount () {
+    const { element } = this.props
+
+    this.calcSize(this.props)
+
+    element.addEventListener('touchstart', this.tap)
+    element.addEventListener('touchmove', this.drag)
+    element.addEventListener('touchend', this.release)
+  }
+
+  componentWillUpdate (nextProps) {
+    this.calcSize(nextProps)
+  }
+
+  componentWillUnmount () {
+    const { element } = this.props
+
+    element.removeEventListener('touchstart', this.tap)
+    element.removeEventListener('touchmove', this.drag)
+    element.removeEventListener('touchend', this.release)
+  }
+
+  calcSize = ({ element }) => {
+    const offset = this.isDirectionVertical() ? element.offsetHeight : element.offsetWidth
+
+    this.max = this.props.max || offset
+    this.min = this.props.min || 0
+  }
+
+  isDirectionVertical = () => {
+    const { direction } = this.props
+
+    return direction === 'y'
+  }
+
+  scroll = pos => {
+    let t = (pos > this.max) ? this.max : (pos < this.min && !this.pressed) ? this.min : pos
+
+    this.offset = t
+
+    this.props.broadcast({position: t, pressed: this.pressed})
+  }
+
+  tap = event => {
+    this.pressed = true
+
+    const { pageY, pageX } = event.touches[0]
+
+    const size = this.isDirectionVertical() ? pageY : pageX
+
+    this.reference = size
+    this.amplitude = this.velocity = 0
+
+    this.frame = this.offset
+
+    clearInterval(this.ticker)
+
+    this.timestamp = Date.now()
+    this.ticker = setInterval(this.track, 50)
+
+    if (this.props.allowTaps === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  drag = event => {
+    if (this.pressed) {
+      const { pageY, pageX } = event.touches[0]
+
+      const pos = this.isDirectionVertical() ? pageY : pageX
+
+      this.delta = this.reference - pos
+
+      if (this.delta > 2 || this.delta < -2) {
+        this.reference = pos
+
+        const r = this.delta + this.offset || this.delta
+
+        this.scroll(r)
+      }
+    }
+
+    if (this.props.allowTaps === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  release = event => {
+    this.pressed = false
+
+    clearInterval(this.ticker)
+
+    if (this.velocity > 10 || this.velocity < -10) {
+      this.amplitude = 0.8 * this.velocity
+
+      this.target = Math.round(this.offset + this.amplitude)
+
+      this.timestamp = Date.now()
+
+      window.requestAnimationFrame(this.autoScroll)
+    }
+
+    this.ticker = null
+
+    if (this.props.allowTaps === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  track = () => {
+    const now = Date.now()
+    this.elapsed = now - this.timestamp
+    this.timestamp = now
+
+    this.delta = this.offset - this.frame
+    this.frame = this.offset
+
+    let v = 1000 * this.delta / (1 + this.elapsed)
+    this.velocity = 0.8 * v + 0.2 * this.velocity
+  }
+
+  autoScroll = () => {
+    if (this.amplitude) {
+      this.elapsed = Date.now() - this.timestamp
+      this.delta = -this.amplitude * Math.exp(-this.elapsed / 325) // ms
+
+      if (this.delta > 0.5 || this.delta < -0.5) {
+        this.scroll(this.target + this.delta)
+
+        // ayy recursion
+        window.requestAnimationFrame(this.autoScroll)
+      } else {
+        this.scroll(this.target)
+      }
+    }
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default Kinetic
---
+++ examples/simple/src/list.js
@@ -0,0 +1,60 @@
+import React, { PureComponent } from 'react'
+
+export default class List extends PureComponent {
+  render () {
+    const { componentStyle } = this.props
+
+    return (
+      <div style={{ ...componentStyle}} ref={r => this.props.getRef(r)}>
+        <div className='list-item'>Alabama</div>
+        <div className='list-item'>Alaska</div>
+        <div className='list-item'>Arizona</div>
+        <div className='list-item'>Arkansas</div>
+        <div className='list-item'>California</div>
+        <div className='list-item'>Colorado</div>
+        <div className='list-item'>Connecticut</div>
+        <div className='list-item'>Delaware</div>
+        <div className='list-item'>Florida</div>
+        <div className='list-item'>Georgia</div>
+        <div className='list-item'>Hawaii</div>
+        <div className='list-item'>Idaho</div>
+        <div className='list-item'>Illinois Indiana</div>
+        <div className='list-item'>Iowa</div>
+        <div className='list-item'>Kansas</div>
+        <div className='list-item'>Kentucky</div>
+        <div className='list-item'>Louisiana</div>
+        <div className='list-item'>Maine</div>
+        <div className='list-item'>Maryland</div>
+        <div className='list-item'>Massachusetts</div>
+        <div className='list-item'>Michigan</div>
+        <div className='list-item'>Minnesota</div>
+        <div className='list-item'>Mississippi</div>
+        <div className='list-item'>Missouri</div>
+        <div className='list-item'>Montana Nebraska</div>
+        <div className='list-item'>Nevada</div>
+        <div className='list-item'>New Hampshire</div>
+        <div className='list-item'>New Jersey</div>
+        <div className='list-item'>New Mexico</div>
+        <div className='list-item'>New York</div>
+        <div className='list-item'>North Carolina</div>
+        <div className='list-item'>North Dakota</div>
+        <div className='list-item'>Ohio</div>
+        <div className='list-item'>Oklahoma</div>
+        <div className='list-item'>Oregon</div>
+        <div className='list-item'>Pennsylvania Rhode Island</div>
+        <div className='list-item'>South Carolina</div>
+        <div className='list-item'>South Dakota</div>
+        <div className='list-item'>Tennessee</div>
+        <div className='list-item'>Texas</div>
+        <div className='list-item'>Utah</div>
+        <div className='list-item'>Vermont</div>
+        <div className='list-item'>Virginia</div>
+        <div className='list-item'>Washington</div>
+        <div className='list-item'>West Virginia</div>
+        <div className='list-item'>Wisconsin</div>
+        <div className='list-item'>Wyoming</div>
+      </div>
+    )
+  }
+}
+
