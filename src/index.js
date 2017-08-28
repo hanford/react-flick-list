46c53f15345944f21ec535f930a276a60cbfb43e
hanford <jackhanford@gmail.com>
2017-08-27 19:50:06 -0700

    less react, more performance

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import document from 'global/document'
 import window from 'global/window'
 
 class Kinetic extends PureComponent {
-  state = {
-    min: 0,
-    max: 0,
-    reference: 0,
-    velocity: 0,
-    offset: 0,
-    amplitude: 0,
-    timestamp: null,
-    ticker: null,
-    pressed: false
-  }
+
+  min = 0
+  max = 0
+  reference = 0
+  velocity = 0
+  offset = 0
+  amplitude = 0
+  timestamp = null
+  ticker = null
+  pressed = false
 
   static defaultProps = {
     broadcast: () => {},
@@ -29,6 +28,8 @@ class Kinetic extends PureComponent {
     const { element } = this.props
 
     this.calcSize(this.props)
+
+    console.log('ELEMENT', element)
 
     element.addEventListener('touchstart', this.tap)
     element.addEventListener('touchmove', this.drag)
@@ -50,7 +51,8 @@ class Kinetic extends PureComponent {
   calcSize = ({ element }) => {
     const offset = this.isDirectionVertical() ? element.offsetHeight : element.offsetWidth
 
-    this.setState({ max: this.props.max || offset, min: this.props.min || 0 })
+    this.max = this.props.max || offset
+    this.min = this.props.min || 0
   }
 
   isDirectionVertical = () => {
@@ -60,29 +62,29 @@ class Kinetic extends PureComponent {
   }
 
   scroll = pos => {
-    const { pressed } = this.state
+    let t = (pos > this.max) ? this.max : (pos < this.min && !this.pressed) ? this.min : pos
 
-    let t = (pos > this.state.max) ? this.state.max : (pos < this.state.min && !pressed) ? this.state.min : pos
+    this.offset = t
 
-    this.setState({ offset: t })
-
-    this.props.broadcast({position: t, pressed})
+    this.props.broadcast({position: t, pressed: this.pressed})
   }
 
   tap = event => {
-    this.setState({ pressed: true })
+    this.pressed = true
 
     const { pageY, pageX } = event.touches[0]
 
     const size = this.isDirectionVertical() ? pageY : pageX
 
-    this.setState({ reference: size, amplitude: 0, velocity: 0 })
+    this.reference = size
+    this.amplitude = this.velocity = 0
 
-    this.state.frame = this.state.offset
+    this.frame = this.offset
 
-    clearInterval(this.state.ticker)
+    clearInterval(this.ticker)
 
-    this.setState({ timestamp: Date.now(), ticker: setInterval(this.track, 50) })
+    this.timestamp = Date.now()
+    this.ticker = setInterval(this.track, 50)
 
     if (this.props.allowTaps === false) {
       event.preventDefault()
@@ -91,19 +93,18 @@ class Kinetic extends PureComponent {
   }
 
   drag = event => {
-    if (this.state.pressed) {
+    if (this.pressed) {
       const { pageY, pageX } = event.touches[0]
 
       const pos = this.isDirectionVertical() ? pageY : pageX
 
-      const { reference } = this.state
-
-      this.delta = reference - pos
+      this.delta = this.reference - pos
 
       if (this.delta > 2 || this.delta < -2) {
-        this.setState({ reference: pos })
+        this.reference = pos
 
-        let r = this.delta + this.state.offset || this.delta
+        const r = this.delta + this.offset || this.delta
+
         this.scroll(r)
       }
     }
@@ -115,21 +116,21 @@ class Kinetic extends PureComponent {
   }
 
   release = event => {
-    this.setState({ pressed: false })
+    this.pressed = false
 
-    clearInterval(this.state.ticker)
+    clearInterval(this.ticker)
 
-    if (this.state.velocity > 10 || this.state.velocity < -10) {
-      this.setState({ amplitude: 0.8 * this.state.velocity })
+    if (this.velocity > 10 || this.velocity < -10) {
+      this.amplitude = 0.8 * this.velocity
 
-      this.target = Math.round(this.state.offset + this.state.amplitude)
+      this.target = Math.round(this.offset + this.amplitude)
 
-      this.setState({ timestamp: Date.now() })
+      this.timestamp = Date.now()
 
       window.requestAnimationFrame(this.autoScroll)
     }
 
-    this.setState({ ticker: null })
+    this.ticker = null
 
     if (this.props.allowTaps === false) {
       event.preventDefault()
@@ -139,28 +140,26 @@ class Kinetic extends PureComponent {
 
   track = () => {
     const now = Date.now()
-    this.elapsed = now - this.state.timestamp
-    this.setState({ timestamp: now })
+    this.elapsed = now - this.timestamp
+    this.timestamp = now
 
-    let delta = this.state.offset - this.state.frame
-    let frame = this.state.offset
+    this.delta = this.offset - this.frame
+    this.frame = this.offset
 
-    let v = 1000 * delta / (1 + this.elapsed)
-    let velocity = 0.8 * v + 0.2 * this.state.velocity
-
-    this.setState({ velocity, frame, delta })
+    let v = 1000 * this.delta / (1 + this.elapsed)
+    this.velocity = 0.8 * v + 0.2 * this.velocity
   }
 
   autoScroll = () => {
-    if (this.state.amplitude) {
-      this.elapsed = Date.now() - this.state.timestamp
-      this.delta = -this.state.amplitude * Math.exp(-this.elapsed / 325) // ms
+    if (this.amplitude) {
+      this.elapsed = Date.now() - this.timestamp
+      this.delta = -this.amplitude * Math.exp(-this.elapsed / 325) // ms
 
       if (this.delta > 0.5 || this.delta < -0.5) {
         this.scroll(this.target + this.delta)
 
         // ayy recursion
-        requestAnimationFrame(this.autoScroll)
+        window.requestAnimationFrame(this.autoScroll)
       } else {
         this.scroll(this.target)
       }
